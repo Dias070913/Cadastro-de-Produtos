cad60c40e80487cfc64496059debb84634ac3b7b
Thiago <thiagodgomesd892@gmail.com>
2025-06-04 08:37:06 -0300

    Desenvolvendo o login do usuário (Não finalizado)

--- ProjetoCadastroDeProdutos/ProjetoCadastroDeProdutos/Repositorio/ProdutoRepositorio.cs
+++ ProjetoCadastroDeProdutos/ProjetoCadastroDeProdutos/Repositorio/ProdutoRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System.Data;
+using ProjetoCadastroDeProdutos.Models;
 
 namespace ProjetoCadastroDeProdutos.Repositorio
 {
--- ProjetoCadastroDeProdutos/ProjetoCadastroDeProdutos/Repositorio/UsuarioRepositorio.cs
+++ ProjetoCadastroDeProdutos/ProjetoCadastroDeProdutos/Repositorio/UsuarioRepositorio.cs
@@ -1,10 +1,38 @@
 ﻿using MySql.Data.MySqlClient;
 using System.Data;
+using ProjetoCadastroDeProdutos.Models;
 
 
 namespace ProjetoCadastroDeProdutos.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public Usuario ObterUsuario(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new("SELECT * FROM Usuarios WHERE Email = @email", conexao);
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Usuario usuario = null;
+                    if (dr.Read())
+                    {
+                        usuario = new Usuario
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = dr["Nome"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            Senha = dr["Senha"].ToString()
+                        };
+                    }
+                }
+            }
+        }
     }
 }
